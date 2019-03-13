9bd68822e3548d88b80134024aac0def3edcdfd5
unknown <kimnhat2000@gmail.com>
2019-03-12 21:48:07 -0400

    working on Form 3

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Icon, Popup, Container, Input } from 'semantic-ui-react';
+import { Form, Icon, Popup, Container, Input, Modal, Button, Header } from 'semantic-ui-react';
 import uuid from 'uuid';
 
 
@@ -7,45 +7,103 @@ class AppForm extends React.Component {
   state = {
     fuzzyTitles: [],
     answer:'',
+    title:'',
+    content:'',
+    warning:'',
+    showConfirm: false
   }
 
   addFuzzyTitle = () => {
     this.setState({ fuzzyTitles: [...this.state.fuzzyTitles, {id: uuid(), value:''}] });
+    console.log(this.state.fuzzyTitles)
   }
 
   removeFuzzyTitle = (titleId) => {
+    console.log(titleId)
     const fuzzyTitles = this.state.fuzzyTitles.filter(title => title.id !== titleId)
     this.setState({ fuzzyTitles })
   }
 
-  onInputValueChange = (e) => {
+  onTitleInput = (e) => {
+    this.setState({ title: e.target.value })
+  }
+
+  onContentInput = (e) => {
+    this.setState({ content: e.target.value })
+  }
+
+  onMatchSearchInput = (e) => {
+    
     const id = Number(e.target.id);
     const fuzzyTitles = this.state.fuzzyTitles.map((value, index) => index === id ? {...value, value: e.target.value} : value)
     this.setState({ fuzzyTitles })
-    console.log(this.state.fuzzyTitles)
+  }
+
+  onFormSubmit = (e) => {
+    const {title, content} = this.state
+    e.preventDefault()
+    if (!title) {
+      this.setState({ showConfirm: true, warning: 'please enter a topic' })
+      return;
+    } else if (!content) {
+      this.setState({ showConfirm: true, warning: 'please enter suitable answers to this topic' })
+      return;
+    } 
   }
 
   render() {
 
-    const fuzzyTitles = this.state.fuzzyTitles && this.state.fuzzyTitles.map((title, index) => {
+    const { title, content, warning, showConfirm, fuzzyTitles} = this.state
+
+    const possibleMatches = fuzzyTitles && fuzzyTitles.map((item, index) => {
       return(
       <Container key = {index}>
-        <Input icon={<Icon name='minus' inverted circular link onClick={() => this.removeFuzzyTitle(title.id)}/>} fluid label={`match search ${index + 1} / ${this.state.fuzzyTitles.length}`} placeholder='title...' 
+        <Input 
+          icon={<Icon name='minus' inverted circular link onClick={() => this.removeFuzzyTitle(item.id)}/>} fluid label={`match search ${index + 1} / ${this.state.fuzzyTitles.length}`} placeholder='title...' 
           id={index}
           type='text'
-          value={title.value}
-          onChange={this.onInputValueChange}
+          value={item.value}
+          onChange={this.onMatchSearchInput}
         />
       </Container>
     )})
     return (
-      <Form>
-          <Form.Input fluid label='Title' placeholder='title...' /> 
-          <Popup trigger={<Icon name='plus' onClick={this.addFuzzyTitle} link/>} content='add fuzzy search' />
-          {fuzzyTitles}
-        <Form.TextArea label='Suitable answer to this question' placeholder='what is the best way to respond to questions?...' />
+      <Form onSubmit={this.onFormSubmit}>
+
+        <Form.Input 
+          fluid label='Title' 
+          placeholder='title...' 
+          value={title} 
+          onChange={this.onTitleInput} 
+        />
+
+        <Popup trigger={<Icon name='plus' onClick={this.addFuzzyTitle} link/>} content='add fuzzy search' />
+
+        {possibleMatches}
+
+        <Form.TextArea 
+          label='Suitable answer to this topic' 
+          placeholder='what is the best way to respond to questions?...' 
+          value={content} 
+          onChange={this.onContentInput} 
+        />
 
         <Form.Button>Submit</Form.Button>
+
+        <Modal open={showConfirm} basic size='small'>
+          <Header icon='warning' content='Missing fields' />
+          <Modal.Content>
+            <p>
+              {warning}
+            </p>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button basic color='red' inverted onClick={() => this.setState({ showConfirm: false })}>
+              <Icon name='remove'/> Sure!
+            </Button>
+          </Modal.Actions>
+        </Modal>
+
       </Form>
     )
   }
